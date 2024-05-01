802d104f6747efb803b5f4669edc1b27df6b9242
EronAlves1996 <eron.alves@rocketmail.com>
2024-04-30 22:36:51 -0300

    feat: create the dto

---
+++ services/products/internal/product/model.go
@@ -0,0 +1,8 @@
+package product
+
+type Product struct {
+	Id       uint32  `db:"id"`
+	Name     string  `validate:"required" json:"name" db:"name"`
+	Price    float64 `validate:"required,gt=0" json:"price" db:"price"`
+	ImageUrl string  `validate:"required" json:"image_url" db:"image_url"`
+}
