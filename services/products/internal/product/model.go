bbc2a39913a4da6b530d2bf621beb63261ae2987
EronAlves1996 <eron.alves@rocketmail.com>
2024-04-30 23:30:11 -0300

    fix: id serialization

--- services/products/internal/product/model.go
+++ services/products/internal/product/model.go
@@ -1,7 +1,7 @@
 package product
 
 type Product struct {
-	Id       uint32  `db:"id"`
+	Id       uint32  `db:"id" json:"id"`
 	Name     string  `validate:"required" json:"name" db:"name"`
 	Price    float64 `validate:"required,gt=0" json:"price" db:"price"`
 	ImageUrl string  `validate:"required" json:"image_url" db:"image_url"`
