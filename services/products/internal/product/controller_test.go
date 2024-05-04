2c81622d1e143c02d32227ae8f2416ba64ac1826
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-03 22:51:10 -0300

    refactor: test

--- services/products/internal/product/controller_test.go
+++ services/products/internal/product/controller_test.go
@@ -1,22 +1,41 @@
-package product_test
+package product
 
 import (
-	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/EronAlves1996/services/products/internal/product"
+	"github.com/gin-gonic/gin"
 )
 
-func TestSerializedFormat(t *testing.T) {
-	ps := product.FetchAllProducts()[0]
-	mps, err := json.Marshal(ps)
+func TestGettingAllProducts(t *testing.T) {
+	old := getAllProducts
+	defer func() { getAllProducts = old }()
 
-	if err != nil {
-		t.Fatal(err)
+	getAllProducts = func() ([]Product, error) {
+		return []Product{
+			{
+				Id:       1,
+				Name:     "Test Product",
+				Price:    20.9,
+				ImageUrl: "./test.jpg",
+			},
+		}, nil
 	}
 
-	found := string(mps)
-	want := `{"id":1,"name":"Test Product","price":20.9,"image_url":"./test.jpg"}`
+	w := httptest.NewRecorder()
+	e := gin.Default()
+	RegisterController(e)
+
+	r, _ := http.NewRequest("GET", "/products", nil)
+	e.ServeHTTP(w, r)
+
+	if w.Code != 200 {
+		t.Fatalf("Status code is different from expected. Want %q, get %q", w.Code, 200)
+	}
+
+	found := w.Body.String()
+	want := `[{"id":1,"name":"Test Product","price":20.9,"image_url":"./test.jpg"}]`
 	if want != found {
 		t.Fatalf("The json marshalling result is different from the expected! Desired is %q, found %q", want, found)
 	}
